0458b47a3707b35b4fce017dab0a22a18b6d6947
jesmarquez <jesmarquezacevedo@yahoo.es>
2017-02-03 16:25:34 -0500

    Agregando javascript cliente

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename')
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var preset = require('babel-preset-es2015');
 
 gulp.task('styles', function(){
 	gulp
@@ -16,4 +20,13 @@ gulp.task('assets', function(){
         .pipe(gulp.dest('public'));
 })
 
-gulp.task('default', ['styles', 'assets']);
+gulp.task('scripts', function (){
+	browserify('./src/index.js')
+		.transform(babel, preset)
+		.bundle()
+		.pipe(source('index.js'))
+		.pipe(rename('app.js'))
+		.pipe(gulp.dest('public'));
+})
+
+gulp.task('default', ['styles', 'assets', 'scripts']);
