8d9f276ceb075b282c0a15819333a8497e27d85b
jesmarquez <jesmarquezacevedo@yahoo.es>
2017-02-03 10:15:17 -0500

    Agregando pug - Vista index inicial

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', function (req, res) {
-  res.send('...iniciando construcción TTdigital...');
+  res.render('index');
 })
 
 app.listen(3000, function () {
