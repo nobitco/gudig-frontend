4b8ea4fec843db142ca77edd8941b193b697f52d
jesmarquez <jesmarquezacevedo@yahoo.es>
2017-02-03 10:24:01 -0500

    Sirviendo archivos estáticos con express

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'pug');
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.render('index');
